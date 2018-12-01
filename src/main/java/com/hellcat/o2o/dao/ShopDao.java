a2619826f768755e71150f2be7339d4c58b051c6
Wang hongguan <2768405641@qq.com>
2018-12-01 22:32:44 +0800

    店铺测试

---
+++ src/main/java/com/hellcat/o2o/dao/ShopDao.java
@@ -0,0 +1,12 @@
+package com.hellcat.o2o.dao;
+
+import com.hellcat.o2o.entity.Shop;
+
+public interface ShopDao {
+    /**
+     * 新增店铺
+     * @param shop
+     * @return
+     */
+    int insertShop(Shop shop);
+}
